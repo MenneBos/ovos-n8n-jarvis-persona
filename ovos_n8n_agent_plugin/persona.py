6a1caa2438d4a8a793330c454647a1ac366aa95b
sfusco <slf@stevenfusco.com>
2025-08-17 13:09:50 -0400

    removing property

--- ovos_n8n_agent_plugin/persona.py
+++ ovos_n8n_agent_plugin/persona.py
@@ -20,14 +20,11 @@ class N8NJarvisPersona(ChatMessageSolver):
                  internal_lang=None):
         """Initialize JARVIS persona with n8n integration"""
         
-        # Don't pass priority to parent, handle it ourselves
+        # Pass all parameters to parent class normally
         super().__init__(config=config, translator=translator, 
-                        detector=detector, priority=100,
+                        detector=detector, priority=priority,
                         enable_tx=enable_tx, enable_cache=enable_cache,
                         internal_lang=internal_lang)
-        
-        # Set our priority after initialization
-        self.priority = priority or 100
         
         # Initialize n8n client and command processor
         self.n8n_client = N8NClient(self.config)
